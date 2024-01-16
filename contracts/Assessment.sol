2e7dbbd73fee0b3b29cde680554f691296548307
radharani0806 <154230390+radharani0806@users.noreply.github.com>
2024-01-16 16:29:17 +0530

    Update Assessment.sol

--- contracts/Assessment.sol
+++ contracts/Assessment.sol
@@ -1,50 +1,42 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-//import "hardhat/console.sol";
-
 contract Assessment {
     address payable public owner;
     uint256 public balance;
-    uint256 public smallCapFund;
-    uint256 public largeCapFund;
 
     event Deposit(uint256 amount);
     event Withdraw(uint256 amount);
-    event InvestedInSmallCap(uint256 amount);
-    event InvestedInLargeCap(uint256 amount);
+    event FundsMultiplied(uint256 oldBalance, uint256 newBalance);
+    event MiniStatement(address account, string action, uint256 amount);
 
     constructor(uint initBalance) payable {
         owner = payable(msg.sender);
         balance = initBalance;
     }
 
-    function getBalance() public view returns(uint256){
+    function getBalance() public view returns (uint256) {
         return balance;
     }
 
     function deposit(uint256 _amount) public payable {
+        require(msg.sender == owner, "You are not the owner of this account");
         uint _previousBalance = balance;
 
-        // make sure this is the owner
-        require(msg.sender == owner, "You are not the owner of this account");
-
-        // perform transaction
         balance += _amount;
 
-        // assert transaction completed successfully
         assert(balance == _previousBalance + _amount);
 
-        // emit the event
         emit Deposit(_amount);
+        emit MiniStatement(msg.sender, "Deposit", _amount);
     }
 
-    // custom error
     error InsufficientBalance(uint256 balance, uint256 withdrawAmount);
 
     function withdraw(uint256 _withdrawAmount) public {
         require(msg.sender == owner, "You are not the owner of this account");
         uint _previousBalance = balance;
+
         if (balance < _withdrawAmount) {
             revert InsufficientBalance({
                 balance: balance,
@@ -52,41 +44,31 @@ contract Assessment {
             });
         }
 
-        // withdraw the given amount
         balance -= _withdrawAmount;
 
-        // assert the balance is correct
         assert(balance == (_previousBalance - _withdrawAmount));
 
-        // emit the event
         emit Withdraw(_withdrawAmount);
+        emit MiniStatement(msg.sender, "Withdrawal", _withdrawAmount);
     }
 
-    function investInSmallCap(uint256 _amount) public {
-        // make sure this is the owner
+    function multiplyFunds() public {
         require(msg.sender == owner, "You are not the owner of this account");
 
-        // invest in small-cap fund
-        smallCapFund += _amount;
+        uint256 oldBalance = balance;
+        balance *= 5;
 
-        // deduct the invested amount from the balance
-        balance -= _amount;
-
-        // emit the investment event
-        emit InvestedInSmallCap(_amount);
+        emit FundsMultiplied(oldBalance, balance);
+        emit MiniStatement(msg.sender, "Funds Multiplied", 0);
     }
 
-    function investInLargeCap(uint256 _amount) public {
-        // make sure this is the owner
-        require(msg.sender == owner, "You are not the owner of this account");
+    function miniStatement() public pure returns (string[] memory) {
+        string[] memory statements = new string[](4);
 
-        // invest in large-cap fund
-        largeCapFund += _amount;
+        statements[0] = "Latest Action: Deposit";
+        statements[1] = "Latest Action: Withdrawal";
+        statements[2] = "Latest Action: Funds Multiplied";
 
-        // deduct the invested amount from the balance
-        balance -= _amount;
-
-        // emit the investment event
-        emit InvestedInLargeCap(_amount);
+        return statements;
     }
 }
